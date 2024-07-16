5843177a3ff1e74435f44a0d2ddd8718bb2097f7
NandiniVishwa05 <nandinivish05@gmail.com>
2024-07-16 19:50:16 +0530

    database connection with node update

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql2');
+const mysql = require('mysql');
+
 const cors = require('cors');
 const nm = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
+const app = express();
 
 app.use(express.json())
 app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "demo",
+    insecureAuth: true
+
+})
+app.get('/info', (req, res) => {
+    const sql = `INSERT INTO faculty VALUES("ANIL", "IT");`;
+    const show = `select * from faculty;`;
+    db.query(show, (err, data) => {
+        if (err)
+        return res.send("error");
+        return  res.send(data);
+       
+    })
+})
+app.listen(4000, () => {
+    console.log("server started at port 4000");
+    db.connect(() => {
+        console.log("Connected!");
+    })
+})
 
 let tp = nm.createTransport({
     host: process.env.SMTP_HOST,
@@ -19,7 +47,7 @@ let tp = nm.createTransport({
 });
 
 app.post('/sendmail', (req, res) => {
-    const {email,otp} = req.body;
+    const { email, otp } = req.body;
     let mailOptions = {
         from: process.env.SMTP_MAIL,
         to: email,
@@ -35,8 +63,6 @@ app.post('/sendmail', (req, res) => {
         }
     })
 })
-
 app.listen(process.env.PORT, () => {
     console.log("server started at port 8000");
 })
-// yaar hasa mt maa soyi nhi h
