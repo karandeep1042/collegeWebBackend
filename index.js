7c3179d998f1635476036d7b0f39ee90d4df5ae3
NandiniVishwa05 <nandinivish05@gmail.com>
2024-07-17 14:00:06 +0530

    database connection with node

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
 const cors = require('cors');
 const nm = require('nodemailer');
@@ -12,27 +12,35 @@ app.use(express.json())
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "demo",
+    host: "database-2.czk2g4e2s0tj.eu-north-1.rds.amazonaws.com",
+    port: 3306,
+    user: "adminroot",
+    password: "adminroot",
+    database: "radavfaculty",
     insecureAuth: true
-
 })
+
 app.get('/info', (req, res) => {
-    const sql = `INSERT INTO faculty VALUES("ANIL", "IT");`;
     const show = `select * from faculty;`;
     db.query(show, (err, data) => {
-        if (err)
-        return res.send("error");
-        return  res.send(data);
-       
+        if (err) {
+            // return res.send("error");
+            console.log(err.message);
+        }
+        else {
+            console.log(data);
+        }
     })
 })
+
 app.listen(4000, () => {
     console.log("server started at port 4000");
-    db.connect(() => {
-        console.log("Connected!");
+    db.connect((err) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log("Connected!");
+        }
     })
 })
 
@@ -46,8 +54,23 @@ let tp = nm.createTransport({
     },
 });
 
+app.get('/verifycredential', (req, res) => {
+    const { email, password } = req.body;
+    const sql = `select * from faculty where fpassword=${password}`
+    let responsedata;
+    db.query(sql, (err, data) => {
+        if (err) {
+            console.log(err.message);
+        }
+        else {
+            res.send(data);
+        }
+    })
+})
+
 app.post('/sendmail', (req, res) => {
-    const { email, otp } = req.body;
+    const { email, otp, password } = req.body;
+    console.log(password);
     let mailOptions = {
         from: process.env.SMTP_MAIL,
         to: email,
@@ -63,6 +86,7 @@ app.post('/sendmail', (req, res) => {
         }
     })
 })
+
 app.listen(process.env.PORT, () => {
     console.log("server started at port 8000");
 })
